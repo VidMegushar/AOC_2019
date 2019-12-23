f547f42a4f23093a18696811f97785a16faf4b4a
Vid Megusar <vid.megusar@ektimo.si>
2019-12-23 10:30:26 +0100

    Added a few lines in advent22.py

--- advent22.py
+++ advent22.py
@@ -2,7 +2,7 @@ def deal_into_new(sez):
     sez.reverse()
     return sez
 
-def cut_n(sez,n):
+def cut(sez,n):
     new_s = sez[n:]
     new_s += sez[:n]
     return new_s
@@ -20,17 +20,30 @@ def deal_with_increment(sez, inc):
     return new_s
 
 
-#s = [0,1,2,3,4,5,6,7,8,9]
-s = [i for i in range(119315717514047)]
-print(s[2020])
+s = [0,1,2,3,4,5,6,7,8,9]
+#s = [i for i in range(119315717514047)]
+#print(s[2020])
+'''
 f = open("input22.txt","r")
 for line in f:
     line = line.split(" ")
     if line[0] == "cut":
-        s = cut_n(s,int(line[1]))
+        s = cut(s,int(line[1]))
     elif line[1] == "into":
         s = deal_into_new(s)
     elif line[1] == "with":
         s = deal_with_increment(s,int(line[3]))
 
 print(s[2020])
+'''
+
+s1 = s.copy()
+s1 = deal_with_increment(s1,3)
+s1 = deal_into_new(s1)
+
+s2 = s.copy()
+s2 = deal_into_new(s2)
+s2 = deal_with_increment(s2,3)
+
+print(s1)
+print(s2)
